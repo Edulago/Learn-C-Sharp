aa746be39dd3b692110407fc689ab752cb25bf7d
Eduardo Lago <edulago173@gmail.com>
2024-08-18 07:52:55 -0300

    Learn C#

---
+++ Array/find the largest element in the array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Progrma
+{
+    public static void Main(string[] args)
+    {
+        int[] array = new int[] { 18, 21, 37, 30, 91, 99, 12};
+
+        int aux = array[0];
+
+        for(int i = 1; i < array.Length; i++)
+        {
+            if(array[i] > aux)
+            {
+                aux = array[i];
+            }
+        }
+        Console.WriteLine(aux);
+    }
+}
